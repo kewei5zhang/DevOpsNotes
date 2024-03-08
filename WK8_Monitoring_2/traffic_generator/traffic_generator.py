5cea500cb5d83111ec729dfc4055b9bc5db83931
kewei5zhang <keweizhang411@gmail.com>
2024-03-08 22:52:43 +1100

    L02 prep

--- WK8_Monitoring_2/traffic_generator/traffic_generator.py
+++ WK8_Monitoring_2/traffic_generator/traffic_generator.py
@@ -2,15 +2,14 @@ from locust import HttpUser, task, between
 
 
 class QuickstartUser(HttpUser):
-    wait_time = between(5, 9)  # will make the simulated users wait between 5 and 9 seconds
+    wait_time = between(5, 9)  # Our class defines a wait_time that will make the simulated users wait between 1 and 5 seconds after each task (see below) is executed.
 
     # @task is the key. For every running user, Locust creates a greenlet (micro-thread), that will call those methods.
-    # @task(5) indicates the weight is 5.
     @task
     def simulation_endpoint(self):
         self.client.get("/simulation")
 
-    @task(5)
+    @task(3) # The weight of this task is 2, which means it will be called twice as often as the other tasks.
     def red_endpoint(self):
         self.client.get("/red")
 
